1d1074a223486dcdf7a80af00b7d4f2249f181d7
Mirza Sisic <mirzasisic57@gmail.com>
2017-12-29 16:57:54 -0500

    Image and fonts
    Add profile component and  google font.

--- my-app/src/components/Menu/Menu.js
+++ my-app/src/components/Menu/Menu.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Menu = () => (
     <div className="item black">
       <ul>
-        <li><h1><a href="default.asp">Mirza Sisic</a></h1></li>
         <li><h2><a href="news.asp">Home</a></h2></li>
         <li><h2><a href="news.asp">About</a></h2></li>
         <li><h2><a href="news.asp">Contact</a></h2></li>
